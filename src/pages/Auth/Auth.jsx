a561a7b2cf49dd3e505742048436cbb5d204a696
abc <duranakyol71@gmail.com>
2023-01-13 11:06:39 +0100

    hinzufügen ProfileModal, update InfoCard

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -11,16 +11,51 @@ const Auth = () => {
           <h5>Explore the ideas throughout the world</h5>
         </div>
       </div>
-      <SignUp />
+      <LogIn />
     </div>
   );
 };
 
+function LogIn() {
+  return (
+    <div className="a-right">
+      <form className="infoForm authForm">
+        <h3>Log In</h3>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            className="infoInput"
+            name="username"
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            className="infoInput"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+
+        <div>
+          <span style={{ fontSize: "12px" }}>
+            Don't have an account Sign up
+          </span>
+          <button className="button infoButton">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 function SignUp() {
   return (
     <div className="a-right">
-      <form action="" className="infoForm">
-        <h3>Sign Up</h3>
+      <form action="" className="infoForm authForm">
+        <h3>Sign up</h3>
         <div>
           <input
             type="text"
@@ -57,6 +92,14 @@ function SignUp() {
             name="confirmPassword"
           />
         </div>
+        <div>
+          <span style={{ fontSize: "12px" }}>
+            Already have a account. Login
+          </span>
+        </div>
+        <button type="submit" className="button infoButton">
+          Sign up
+        </button>
       </form>
     </div>
   );
